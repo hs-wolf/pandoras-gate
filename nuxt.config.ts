5adf48f91bbf5e9e7e12fcb3bd88ecbdb53948ad
Josh Israel <uno.braid@gmail.com>
2023-07-05 20:12:15 -0300

    feat(app): Installed the @vueuse/core and @vueuse/nuxt modules for essential Vue composition utilities.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,5 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
   },
-  modules: ['@nuxtjs/robots', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/robots', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
 });
