2794fc2ab62333adceb6c1567cd975d8dd82ab5f
Josh Israel <uno.braid@gmail.com>
2023-07-05 19:37:22 -0300

    infra(app): Created the initial Nuxt3 app and configured the initial settings.

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'Pandoras Gate',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Pandoras Gate TTRPG' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+});
