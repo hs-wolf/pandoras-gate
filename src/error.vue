204a6eb4b7fcea424388d31dba96ccc7d67004f7
Josh Israel <uno.braid@gmail.com>
2023-07-05 22:10:03 -0300

    feat(app): Created the error page.

---
+++ src/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <h1>Error Page</h1>
+    <p>{{ error?.message }}</p>
+    <button @click="handleError" class="font-bold">Clear Error</button>
+  </div>
+</template>
